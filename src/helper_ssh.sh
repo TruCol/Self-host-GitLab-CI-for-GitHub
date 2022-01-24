05784c7326ac99673617246cf563ea8d12821fd4
test test <testaccount@testaccount.testaccount>
2022-01-24 19:39:06 +0100

    Refactored helper.sh.

--- src/helper_ssh.sh
+++ src/helper_ssh.sh
@@ -45,3 +45,170 @@ has_access() {
 	#echo $(check_ssh_access_to_repo "$GITHUB_USERNAME_GLOBAL" "$github_repo")
 	check_ssh_access_to_repo "$GITHUB_USERNAME_GLOBAL" "$github_repo"
 }
+
+
+#######################################
+# 
+# Local variables:
+# 
+# Globals:
+#  None.
+# Arguments:
+#   
+# Returns:
+#  0 if 
+#  7 if 
+# Outputs:
+#  None.
+# TODO(a-t-0): change root with Global variable.
+#######################################
+# Structure:Github_status
+# Structure:ssh
+# Returns FOUND if the incoming ssh account is activated,
+# returns NOTFOUND otherwise.
+github_account_ssh_key_is_added_to_ssh_agent() {
+	local ssh_account="$1"
+	local activated_ssh_output=("$@")
+	found="false"
+	
+	
+	count=0
+	while IFS= read -r line; do
+		count=$((count+1))
+		
+		# Get the username from the ssh key .pub file.
+		local username
+		username="$(get_last_space_delimted_item_in_line "$line")"
+		
+		if [ "$username" == "$ssh_account" ]; then
+			if [ "$found" == "false" ]; then
+				echo "FOUND"
+				found="true"
+			fi
+		fi
+
+	done <<< "${activated_ssh_output[@]}"
+	if [ "$found" == "false" ]; then
+		echo "NOTFOUND"
+	fi
+}
+
+
+#######################################
+# 
+# Local variables:
+# 
+# Globals:
+#  None.
+# Arguments:
+#   
+# Returns:
+#  0 if 
+#  7 if 
+# Outputs:
+#  None.
+# TODO(a-t-0): change root with Global variable.
+#######################################
+# Structure:ssh
+# Checks for both GitHub username as well as for the email address that is 
+# tied to that acount.
+any_ssh_key_is_added_to_ssh_agent() {
+	local ssh_account=$1
+	local ssh_output
+	ssh_output=$(ssh-add -L)
+	
+	# Check if the ssh key is added to ssh-agent by means of username.
+	found_ssh_username="$(github_account_ssh_key_is_added_to_ssh_agent "$ssh_account" "\"${ssh_output}")"
+	if [[ "$found_ssh_username" == "FOUND" ]]; then
+		echo "FOUND"
+	else
+		
+		# Get the email address tied to the ssh-account.
+		ssh_email=$(get_ssh_email "$ssh_account")
+		#echo "ssh_email=$ssh_email"
+		
+		if [ "$ssh_email" == "" ]; then
+			#echo "The ssh key file does not exist, so the email address of that ssh-account can not be extracted."
+			echo "NOTFOUND_FILE"
+			exit 27
+		else 
+			
+			# Check if the ssh key is added to ssh-agent by means of email.
+			found_ssh_email="$(github_account_ssh_key_is_added_to_ssh_agent "$ssh_email" "\"${ssh_output}")"
+			
+			if [ "$found_ssh_email" == "FOUND" ]; then
+				echo "FOUND"
+			else
+				#manual_assert_equal  "$found_ssh_email" "FOUND"
+				echo "NOTFOUND_EMAIL"
+			fi
+		fi
+	fi
+}
+
+
+#######################################
+# 
+# Local variables:
+# 
+# Globals:
+#  None.
+# Arguments:
+#   
+# Returns:
+#  0 if 
+#  7 if 
+# Outputs:
+#  None.
+# TODO(a-t-0): change root with Global variable.
+#######################################
+# Structure:ssh
+verify_ssh_key_is_added_to_ssh_agent() {
+	local ssh_account=$1
+	local ssh_output
+	ssh_output=$(ssh-add -L)
+	local ssh_key_in_ssh_agent
+	ssh_key_in_ssh_agent=$(any_ssh_key_is_added_to_ssh_agent "$ssh_account")
+	if [[ "$ssh_key_in_ssh_agent" == "NOTFOUND_FILE" ]] || [[ "$ssh_key_in_ssh_agent" == "NOTFOUND_EMAIL" ]]; then
+		printf 'Please ensure the ssh-account '%ssh_account' key is added to the ssh agent. You can do that with commands:'"\\n"" eval $(ssh-agent -s)""\n"'ssh-add ~/.ssh/'$ssh_account''"\n"' Please run this script again once you are done.'
+		exit 28
+	fi
+}
+
+
+#######################################
+# 
+# Local variables:
+# 
+# Globals:
+#  None.
+# Arguments:
+#   
+# Returns:
+#  0 if 
+#  7 if 
+# Outputs:
+#  None.
+# TODO(a-t-0): change root with Global variable.
+#######################################
+# Structure:ssh
+# Untested function to retrieve email pertaining to ssh key
+get_ssh_email() {
+	local ssh_account=$1
+	
+	local username
+	username=$(whoami)
+	local key_filepath="/home/$username/.ssh/$ssh_account.pub"
+	
+	# Check if file exists.
+	manual_assert_file_exists "$key_filepath"
+	
+	# Read the ssh pub file.
+	local public_ssh_content
+	public_ssh_content=$(cat "$key_filepath")
+	
+	# Get email from ssh pub file.
+	local email
+	email=$(get_last_space_delimted_item_in_line "$public_ssh_content")
+	echo "$email"
+}
--- src/import.sh
+++ src/import.sh
@@ -32,6 +32,12 @@ source src/helper_gitlab_modify.sh
 source src/helper_gitlab_status.sh
 source src/helper_git_neutral.sh
 source src/helper_ssh.sh
+source src/helper.sh
+source src/helper_docker.sh
+source src/helper_parsing.sh
+source src/helper_configuration.sh
+source src/helper_file_dir_related.sh
+
 
 # For uninstallation
 source src/uninstall_gitlab_server.sh
@@ -48,7 +54,7 @@ source src/get_gitlab_server_runner_token.sh
 source src/run_ci_on_github_repo.sh
 
 
-source src/helper.sh
+
 source src/sha256_computing.sh
 
 

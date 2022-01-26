603cdb184cbdfa3a5d380d42363b12e2fc129116
test test <testaccount@testaccount.testaccount>
2022-01-26 20:26:22 +0100

    Identified todo's.

--- test.sh
+++ test.sh
@@ -21,28 +21,32 @@
 
 
 ############### no_server_required - preserves_server##########################
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_logging.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_boot_tor.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_docker.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_parsing.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_file_dir_related.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_support_programs.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_uninstall.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_install_and_boot_gitlab_server.bats
-#./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_helper_dir_edit.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_logging.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_boot_tor.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_docker.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_parsing.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_file_dir_related.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_support_programs.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_uninstall.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_install_and_boot_gitlab_server.bats
+./test/libs/bats/bin/bats test/no_server_required/preserves_server/test_helper_dir_edit.bats
 
 ############### no_server_required - breaks_server##########################
 # Works (Takes 45 sec)
+# TODO: move into right folder
 ./test/libs/bats/bin/bats test/test_helper_docker.bats
 exit 1
 
 ############### server_required - preserves_server##########################
 ### Partially working (requires installation)
+# TODO: separate into: requires installation, does not require installation.
 ./test/libs/bats/bin/bats test/server_required/preserves_server/test_helper_github_status.bats
 
-# FAIL
+# Fails
 ./test/libs/bats/bin/bats test/server_required/breaks_server/test_docker.bats
 # New test
+# TODO: move into right folder
+# TODO: (if needed) separate into: requires installation, does not require installation.
 ./test/libs/bats/bin/bats test/test_run_ci_on_github_repo.bats
 ############### server_required - breaks_server##########################
 
@@ -67,8 +71,5 @@ exit 1
 ###./test/libs/bats/bin/bats test/test_sha256_checksum.bats
 
 # New test
-
 ###./test/libs/bats/bin/bats test/test_helper_asserts.bats
 
-
-# Long tests (passing)

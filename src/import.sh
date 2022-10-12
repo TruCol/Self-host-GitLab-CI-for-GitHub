f23bdcbda910b1d31bb0d5c3a45d7dbadce415d0
Your Name <you@example.com>
2022-10-12 13:39:08 +0200

    Updated import function.

--- src/import.sh
+++ src/import.sh
@@ -65,9 +65,10 @@ source src/prerequisites/install_support_programs.sh
 source src/Selenium/create_gitlab_personal_access_token.sh # TODO: verify its naming.
 source src/Selenium/create_github_personal_access_token.sh
 source src/Selenium/get_gitlab_server_runner_token.sh
-source src/Selenium//SSH/ensure_GitHub_ssh_access.sh
-source src/Selenium/SSH/local_ssh_checks.sh
 source src/Selenium/SSH/check_GitHub_ssh_access.sh
+source src/Selenium/SSH/ensure_GitHub_ssh_access.sh
+source src/Selenium/SSH/local_ssh_checks.sh
+source src/Selenium/SSH/set_GitHub_ssh_key.sh
 
 # For uninstallation
 source src//GitLab_server/uninstall_gitlab_server.sh
